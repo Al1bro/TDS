3449a4ec65b11d556cc1977f3c6ce40ef90d5e9d
Al1bro <102135299+Al1bro@users.noreply.github.com>
2023-01-12 21:07:41 +0500

    lab7

---
+++ src/LR7/Ex5/subClass1.java
@@ -0,0 +1,24 @@
+package LR7.Ex5;
+
+public class subClass1 extends superClass {
+    protected int chisl;
+
+    subClass1(String strEx, int intEx){
+        super(strEx);
+        this.chisl = intEx;
+    }
+
+    @Override
+    public String toString() {
+        String ClassNameAndFieldValue;
+        ClassNameAndFieldValue = "Вывод: " + "\n" +
+                "Имя класса: " + this.getClass().getSimpleName() + "\n" +
+                "Текст: " + this.getStr() + "\n" +
+                "Число: " + this.getInt() + "\n";
+        return ClassNameAndFieldValue ;
+    }
+
+    public int getInt(){
+        return chisl;
+    }
+}
