1c6173247aaafef52e7ecaae26242bb405441009
Al1bro <102135299+Al1bro@users.noreply.github.com>
2023-01-21 02:59:48 +0500

    OPD

--- src/OPD/lr5.java
+++ src/OPD/lr5.java
@@ -12,24 +12,48 @@ public class lr5 {
             int qobjeCTsi = user.nextInt();
             System.out.println("Введите количество новых или модифицируемых бизнес методов: ");
             int qMeth = user.nextInt();
-            double EUI = ,EDIANE(2, 4, 20);
+            System.out.println("Введите минимальные трудозатраты для одного экрана: ");
+            int min1 = user.nextInt();
+            System.out.println("Введите минимальные трудозатраты для одного обработчика данных: ");
+            int min2 = user.nextInt();
+            System.out.println("Введите минимальные трудозатраты для нового бизнес-объекта: ");
+            int min3 = user.nextInt();
+            System.out.println("Введите минимальные трудозатраты для добавления нового бизнес-метода: ");
+            int min4 = user.nextInt();
+            System.out.println("Введите наиболее вероятные трудозатраты для одного экрана: ");
+            int most1 = user.nextInt();
+            System.out.println("Введите наиболее вероятные трудозатраты для одного обработчика данных: ");
+            int most2 = user.nextInt();
+            System.out.println("Введите наиболее вероятные трудозатраты для нового бизнес-объекта: ");
+            int most3 = user.nextInt();
+            System.out.println("Введите наиболее вероятные трудозатраты для добавления нового бизнес-метода: ");
+            int most4 = user.nextInt();
+            System.out.println("Введите максимальные трудозатраты для одного экрана: ");
+            int max1 = user.nextInt();
+            System.out.println("Введите максимальные трудозатраты для одного обработчика данных: ");
+            int max2 = user.nextInt();
+            System.out.println("Введите максимальные трудозатраты для нового бизнес-объекта: ");
+            int max3 = user.nextInt();
+            System.out.println("Введите максимальные трудозатраты для добавления нового бизнес-метода: ");
+            int max4 = user.nextInt();
+            double EUI = ,EDIANE(min1, most1, max1);
             System.out.printf("\nСредняя трудоемкость кодирования пользовательского экрана равна %.2f чел.*час\n", EUE);
-            double EACT = MEDIANE(4, 8, 32);
+            double EACT = MEDIANE(min2, most2, max2);
             System.out.printf("Средняя трудоемкость кодирования одного обработчика событий равна %.2f чел.*час\n", EACT);
-            double EBO = MEDIANE(2, 3, 8);
+            double EBO = MEDIANE(min3, most3, max3);
             System.out.printf("Средняя трудоемкость кодирования нового бизнес-объекта равна %.2f чел.*час\n", EBO);
-            double EBM = MEDIANE(2, 6, 26);
+            double EBM = MEDIANE(min4, most4, max4);
             System.out.printf("Средняя трудоемкость кодирования нового бизнес-метода равна %.2f чел.*час\n\n", EBM);
-            double CI = CKO(2, 20);
+            double CI = CKO(min1, max1);
             System.out.printf("Среднеквадратичное отклонение кодирования пользовательского экрана " +
                     "равна %.2f чел.*час\n", CI);
-            double CT = CKO(4, 32);
+            double CT = CKO(min2, max2);
             System.out.printf("Среднеквадратичное отклонение кодирования одного обработчика событий " +
                     "равна %.2f чел.*час\n", CT);
-            double CB = CKO(2, 8);
+            double CB = CKO(min3, max3);
             System.out.printf("Среднеквадратичное отклонение кодирования нового бизнес-объекта " +
                     "равна %.2f чел.*час\n", CB);
-            double CKOBM = CKO(2,26);
+            double CKOBM = CKO(min4,max4);
             System.out.printf("Среднеквадратичное отклонение кодирования нового бизнес-метода " +
                     "равна %.2f чел.*час\n\n", CKOBM);
             double ETL = EUI * UI + EACT * ACT + EBO * objeCTsi + EBM * qMeth;
@@ -49,7 +73,7 @@ public class lr5 {
             double MANMONTHTOTALE = WPROJECT / 132;
             System.out.printf("Общая трудоемкость проекта равна %.2f чел.*мес.\n", MANMONTHTOTALE);
             double PLENGTH = 2.5 * Math.pow(MANMONTHTOTALE, 1./3);
-            System.out.printf("Оптимальная продолжительность проекта равна %.1f месяцев\n", PLENGTH);
+            System.out.printf("Оптимальная продолжительность проекта равна %.2f месяцев\n", PLENGTH);
             double CREWQUANTITY = MANMONTHTOTALE / PLENGTH;
             System.out.printf("Средняя численность команды равна %.2f человек", CREWQUANTITY);
         }
